1a1ae176fbe90858693cce0ed0a9fabdd8ee42a1
Pasillas-Kim <masterofpoppies@gmail.com>
2025-12-20 07:31:23 +0000

    feat: solve regex problem 003

---
+++ regex/003_a_followed_by_1+bs.py
@@ -0,0 +1,15 @@
+"""
+Write a Python program that matches a string that has an a followed by zero or more b's.
+"""
+
+import re
+
+def is_a_0p_bs(string_to_check: str) -> bool:
+    p = re.compile('^ab+$')
+    return bool(p.match(string_to_check))
+
+if __name__ == '__main__':
+    print(f"abc : {is_a_0p_bs('abc')}")
+    print(f"a : {is_a_0p_bs('a')}")
+    print(f"abb : {is_a_0p_bs('abb')}")
+    print(f"bbbb : {is_a_0p_bs('bbbb')}")
