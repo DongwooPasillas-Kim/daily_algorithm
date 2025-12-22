22ff7341572fa14584a117a5b2fd0ae9680eb835
Dongwoo Pasillas-Kim <dongwookim1027@gmail.com>
2025-12-22 22:15:29 +0900

    feat: study javascripts object methods

---
+++ javascript/003_object_method.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const user = {
+    name : 'Mike',
+    age : 30
+}
+
+let cloneUser = Object.assign({}, user);
+console.log(`cloneUser : ${cloneUser}`);
+console.log(`To get keys, use keys(): ${Object.keys(cloneUser)}`);
+console.log(`To get values, use values(): ${Object.values(cloneUser)}`);
+console.log(`To get all, use entries(): ${Object.entries(cloneUser)}`);
+
+let arr = [
+    ['Monday', 'Pizza'],
+    ['Tuesay', 'Hamburger']
+]
+let result = Object.fromEntries(arr);
+console.log(result);
