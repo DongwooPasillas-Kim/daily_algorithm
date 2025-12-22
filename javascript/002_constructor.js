6a692fc5f3042376d5d0ab9fe3c909cf8a936c83
Dongwoo Pasillas-Kim <dongwookim1027@gmail.com>
2025-12-22 22:01:16 +0900

    feat: study javascript constructor

---
+++ javascript/002_constructor.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function User(name, age){
+    // this = {}
+    this.name = name;
+    this.age = age;
+    // return this;
+}
+
+let myUser = new User("Jason", 51);
+console.log(myUser)
+
+// Computed property
+
+let feature1 = 'name';
+
+const monster = {
+    [feature1] : 'Mike',
+    age : 30
+}
+
+console.log(monster)
